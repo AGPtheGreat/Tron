15f260d374094c64111f36c9530acc8b3063bc81
agp92 <agp92@Eclipse>
2025-10-06 15:06:24 -0500

    basic menu (buttons don't work yet)

--- Tron/src/menu/MainMenu.java
+++ Tron/src/menu/MainMenu.java
@@ -6,6 +6,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
 
 public class MainMenu extends JFrame {
 
@@ -32,11 +37,38 @@ public class MainMenu extends JFrame {
 	 * Create the frame.
 	 */
 	public MainMenu() {
+		setBackground(Color.WHITE);
+		setTitle("Main Menu");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 700, 500);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.NORTH);
+		
+		JLabel GameName = new JLabel("Tron");
+		GameName.setFont(new Font("Tahoma", Font.PLAIN, 32));
+		panel.add(GameName);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.SOUTH);
+		
+		JPanel playLbl = new JPanel();
+		panel_1.add(playLbl);
+		
+		JButton playBtn = new JButton("Play");
+		playBtn.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		playLbl.add(playBtn);
+		
+		JPanel quitLbl = new JPanel();
+		panel_1.add(quitLbl);
+		
+		JButton btnNewButton = new JButton("Quit");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		quitLbl.add(btnNewButton);
 
 	}
 
